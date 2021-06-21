49632d46392acf1f160c4ce6891b7941da75c998
Mikle <gumenyukmichael@gmail.com>
2021-06-21 20:37:29 +0300

    2. Каркас шахматной доски [#792] Add test

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -15,4 +15,30 @@ public class LogicTest {
         logic.add(new BishopBlack(Cell.C1));
         logic.move(Cell.C1, Cell.H6);
     }
+
+    @Test(expected = ImpossibleMoveException.class)
+    public void whenStrangeMove() throws ImpossibleMoveException, OccupiedCellException, FigureNotFoundException {
+        Logic logic = new Logic();
+        BishopBlack bishopBlack = new BishopBlack(Cell.A1);
+        logic.add(bishopBlack);
+        logic.move(bishopBlack.position(), Cell.findBy(0, 6));
+    }
+
+    @Test(expected = FigureNotFoundException.class)
+    public void whenOutCell() throws ImpossibleMoveException, OccupiedCellException, FigureNotFoundException {
+        Logic logic = new Logic();
+        BishopBlack bishopBlack = new BishopBlack(Cell.A1);
+        logic.add(bishopBlack);
+        logic.move(Cell.findBy(-100, -2), Cell.findBy(0, 6));
+    }
+
+    @Test(expected = OccupiedCellException.class)
+    public void whenGoToNotEmptyCell() throws ImpossibleMoveException, OccupiedCellException, FigureNotFoundException {
+        Logic logic = new Logic();
+        BishopBlack bishopBlack = new BishopBlack(Cell.A1);
+        BishopBlack bishopBlackSec = new BishopBlack(Cell.B2);
+        logic.add(bishopBlack);
+        logic.add(bishopBlackSec);
+        logic.move(bishopBlack.position(), Cell.C3);
+    }
 }
